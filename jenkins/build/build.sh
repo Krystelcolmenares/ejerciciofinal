ba73fcf133f3a4d8a104989494e46a91d1ecd3b7
Krystelcolmenares <32401934+Krystelcolmenares@users.noreply.github.com>
2020-10-09 19:50:06 -0300

    Add files via upload

---
+++ jenkins/build/build.sh
@@ -0,0 +1,5 @@
+cp -f java-app/target/*.jar jenkins/build/
+echo "######################"
+echo "*** Building image ***"
+echo "######################"
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
